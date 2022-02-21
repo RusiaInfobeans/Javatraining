103bc1e3993c8a90a160d3d99bc54a37c2d68e22
rusia7381 <99735811+rusia7381@users.noreply.github.com>
2022-02-21 15:20:53 +0530

    some code update

---
+++ day6/CheckAnagrmUsingaHashmap.java
@@ -0,0 +1,54 @@
+package day6;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CheckAnagrmUsingaHashmap {
+    static boolean areAnagram(String str1, String str2)
+    {
+        HashMap<Character, Integer> hmap1 = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> hmap2 = new HashMap<Character, Integer>();
+
+        char arr1[] = str1.toCharArray();
+        char arr2[] = str2.toCharArray();
+        for (int i = 0; i < arr1.length; i++) {
+            if (hmap1.get(arr1[i]) == null) {
+                hmap1.put(arr1[i], 1);
+            }
+            else {
+                Integer c = (int)hmap1.get(arr1[i]);
+                hmap1.put(arr1[i], ++c);
+            }
+        }
+        for (int j = 0; j < arr2.length; j++) {
+
+            if (hmap2.get(arr2[j]) == null)
+                hmap2.put(arr2[j], 1);
+            else {
+
+                Integer d = (int)hmap2.get(arr2[j]);
+                hmap2.put(arr2[j], ++d);
+            }
+        }
+
+        if (hmap1.equals(hmap2))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter to Strings:");
+        String str1=scanner.nextLine();
+        String str2=scanner.nextLine();
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+        if (areAnagram(str1, str2))
+            System.out.println("The two string Are anagram");
+        else
+            System.out.println("The two strings are not angram");
+    }
+}
